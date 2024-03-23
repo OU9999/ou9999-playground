7b60f63a01fd6b0cc51ff23bedfffd2c495fa47b
오유진 <omh232323@gmail.com>
2024-03-23 23:50:55 +0900

    [feat] header icon
    1. 헤더 아이콘 설정
    2. 헤더 아이콘, 뱃지 그레디언트 설정

--- src/components/headSection/content-header.tsx
+++ src/components/headSection/content-header.tsx
@@ -10,7 +10,7 @@ const ContentHeader = ({ title, text, badge }: ContentHeaderProps) => {
   return (
     <>
       {badge && (
-        <Badge className="cursor-pointer bg-gradient-to-r  hover:text-slate-400">
+        <Badge className="cursor-pointer font-bold bg-gradient-to-r text-slate-900 from-[#C0E4DF] to-[#BBBEE9] hover:text-slate-700">
           {badge}
         </Badge>
       )}
--- src/components/nav/header.tsx
+++ src/components/nav/header.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
+import OwlIcon from "../svg/owl-icon";
 
 const Header = () => {
   return (
     <header className="z-40 w-full h-14 flex justify-center fixed bg-blur-black border-b-1 border-slate-800 backdrop-blur-md">
       <div className="w-full max-w-336 px-5 h-full flex justify-between items-center">
+        <div className="flex space-x-1">
+          <div className="w-10 h-10">
+            <OwlIcon />
+          </div>
+          <p className="font-bold text-2xl relative top-[2px]">PLAYGROUND</p>
+        </div>
         <div className="flex space-x-2">
           <Link href={"/"}>
             <Button>Home</Button>
