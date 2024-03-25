42617785f31769c2940a26607bc33cb8dcaf888d
오유진 <omh232323@gmail.com>
2024-03-25 23:15:00 +0900

    [feat] tabs

--- src/app/(home-layout)/page.tsx
+++ src/app/(home-layout)/page.tsx
@@ -1,7 +1,19 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const HomePage = () => {
   return (
     <div className="w-full flex flex-col">
-      <p>this is home</p>
+      <Tabs defaultValue="all" className="w-full">
+        <TabsList className="bg-transparent space-x-3">
+          <TabsTrigger value="all">ALL</TabsTrigger>
+          <TabsTrigger value="ui">UI/UX</TabsTrigger>
+          <TabsTrigger value="engineering">Engineering</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="all">all</TabsContent>
+        <TabsContent value="ui">UI/UX</TabsContent>
+        <TabsContent value="engineering">Engineering</TabsContent>
+      </Tabs>
     </div>
   );
 };
