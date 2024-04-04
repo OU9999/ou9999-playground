a9829ca923e9cad004d9986e9176872030d6089f
오유진 <omh232323@gmail.com>
2024-04-05 02:52:28 +0900

    [feat] 기본 open graph 설정

--- src/app/(detail-layout)/detail/ai-tti/page.tsx
+++ src/app/(detail-layout)/detail/ai-tti/page.tsx
@@ -1,4 +1,20 @@
 import AiTextToImage from "@/components/mainSection/ai-tti/ai-text-to-image";
+import { getSectionDataByLink } from "@/util/section-data-util";
+
+export const generateMetadata = () => {
+  const sectionData = getSectionDataByLink("ai-tti");
+  const title = `${sectionData?.title} | ou`;
+  const description = `${sectionData?.description} | ou`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+    },
+  };
+};
 
 const AiTtiDetailPage = () => {
   return (
--- src/app/(detail-layout)/detail/cross-fade/page.tsx
+++ src/app/(detail-layout)/detail/cross-fade/page.tsx
@@ -1,4 +1,20 @@
 import CrossFadeTransition from "@/components/mainSection/cross-fade/cross-fade-transition";
+import { getSectionDataByLink } from "@/util/section-data-util";
+
+export const generateMetadata = () => {
+  const sectionData = getSectionDataByLink("cross-fade");
+  const title = `${sectionData?.title} | ou`;
+  const description = `${sectionData?.description} | ou`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+    },
+  };
+};
 
 const CrossFadeDetailPage = () => {
   return (
