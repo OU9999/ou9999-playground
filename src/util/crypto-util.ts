bc0d935a5c90067cd83065732e48cc6dd178fa83
오유진 <omh232323@gmail.com>
2024-04-05 03:19:32 +0900

    [hotfix] cryptojs error fix

--- src/util/crypto-util.ts
+++ src/util/crypto-util.ts
@@ -1,20 +1,20 @@
 import CryptoJS from "crypto-js";
 
-export const encrypt = (data: string) => {
-  const key = process.env.NEXT_PUBLIC_CRYPTO_JS_KEY;
-  const encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), key!);
-  return encrypted.toString();
-};
+// export const encrypt = (data: string) => {
+//   const key = process.env.NEXT_PUBLIC_CRYPTO_JS_KEY;
+//   const encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), key!);
+//   return encrypted.toString();
+// };
 
-export const decrypt = (text: string) => {
-  try {
-    const key = process.env.NEXT_PUBLIC_CRYPTO_JS_KEY;
-    const bytes = CryptoJS.AES.decrypt(text, key!);
-    const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+// export const decrypt = (text: string) => {
+//   try {
+//     const key = process.env.NEXT_PUBLIC_CRYPTO_JS_KEY;
+//     const bytes = CryptoJS.AES.decrypt(text, key!);
+//     const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
 
-    return decryptedData;
-  } catch (err) {
-    console.log(err);
-    return;
-  }
-};
+//     return decryptedData;
+//   } catch (err) {
+//     console.log(err);
+//     return;
+//   }
+// };
