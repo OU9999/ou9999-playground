637d7e20472590abd7141d4765cb64ebbd8af793
오유진 <omh232323@gmail.com>
2024-04-05 02:46:00 +0900

    [update] 테스트 gridbox 삭제

--- src/app/(detail-layout)/detail/cross-fade/page.tsx
+++ src/app/(detail-layout)/detail/cross-fade/page.tsx
@@ -1,11 +1,9 @@
 import CrossFadeTransition from "@/components/mainSection/cross-fade/cross-fade-transition";
-import GridBox from "@/components/ui/grid-box";
 
 const CrossFadeDetailPage = () => {
   return (
     <div className="w-full h-auto min-h-dvh">
       <CrossFadeTransition />
-      <GridBox className="h-96">1231231231</GridBox>
     </div>
   );
 };
