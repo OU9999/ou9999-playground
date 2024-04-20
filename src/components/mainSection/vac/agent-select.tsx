9d11a8e66692d6c67e2e0d391323750a9cc70e05
오유진 <omh232323@gmail.com>
2024-04-21 02:15:04 +0900

    [update] vac color 수정
    1. 네온, 레이나 그레디언트 색상 수정
    
    2. border 굵기 수정

--- src/components/mainSection/vac/agent-select.tsx
+++ src/components/mainSection/vac/agent-select.tsx
@@ -9,10 +9,10 @@ const agentData = [
   { name: "jett", from: "#5A8E9B", to: "#21324B" },
   { name: "viper", from: "#5C9655", to: "#121B1E" },
   { name: "astra", from: "#96558B", to: "#29114F" },
-  { name: "neon", from: "#645292", to: "#353074" },
+  { name: "neon", from: "#6B569D", to: "#1E1D43" },
   { name: "omen", from: "#394B7D", to: "#1F253C" },
   { name: "phoenix", from: "#85683E", to: "#8C4A3D" },
-  { name: "reyna", from: "#723E49", to: "#7D3A6F" },
+  { name: "reyna", from: "#7F434F", to: "#2C1839" },
   { name: "sage", from: "#489587", to: "#274953" },
   { name: "skye", from: "#609060", to: "#524435" },
   { name: "sova", from: "#957D6B", to: "#14192A" },
@@ -87,7 +87,7 @@ const AgentSelect = () => {
         ))}
 
         <div className="absolute w-full h-32 bottom-20 z-30 px-8">
-          <div className="w-full h-full border-t-2 border-slate-800 border-opacity-60 flex justify-center py-3">
+          <div className="w-full h-full border-t-4 border-slate-700 border-opacity-60 flex justify-center py-3">
             <div className="w-full h-auto">
               <div ref={emblaThumbsRef} className="overflow-hidden">
                 <div className="w-auto flex flex-row pl-2 space-x-2">
