0a4ad16e40d9bee6b97d643f6226ae607e32dc65
오유진 <omh232323@gmail.com>
2024-06-18 18:09:22 +0900

    [update] ai-tti 구현 과정 아코디언 적용

--- src/app/(detail-layout)/detail/ai-tti/page.tsx
+++ src/app/(detail-layout)/detail/ai-tti/page.tsx
@@ -1,4 +1,6 @@
 import AiTextToImage from "@/components/mainSection/ai-tti/ai-text-to-image";
+import AiProgress from "@/components/mainSection/ai-tti/ai-tti-progress";
+
 import { getMetaDataFromSectionData } from "@/util/meta-data-util";
 import { getSectionDataByLink } from "@/util/section-data-util";
 import Link from "next/link";
@@ -26,13 +28,7 @@ const AiTtiDetailPage = () => {
   return (
     <div className="w-full h-auto min-h-dvh">
       <AiTextToImage />
-      <p className="mt-10 mb-5 text-2xl">구현과정</p>
-
-      <Link href={"https://ou9999-dev.com/p/server-action-with-replicate"}>
-        <p className="underline cursor-pointer hover:text-slate-300">
-          NEXT.JS 서버 액션으로 AI TEXT-TO-IMAGE 구현하기
-        </p>
-      </Link>
+      <AiProgress />
     </div>
   );
 };
