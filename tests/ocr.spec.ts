722e0e8db6027378371adb562806472bf76e0503
오유진 <omh232323@gmail.com>
2024-07-25 23:46:10 +0900

    [feat] ocr 기능 세분화 테스트

--- tests/ocr.spec.ts
+++ tests/ocr.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("ocr 기능 테스트", async ({ page }) => {
+test("ocr 기능 테스트 - 신분증", async ({ page }) => {
   // index 페이지에서 시작 (playwright.config.ts에서 webServer를 통해 baseURL을 설정.)
   await page.goto("/");
 
@@ -21,4 +21,27 @@ test("ocr 기능 테스트", async ({ page }) => {
   // 결과, 이름, 주민등록번호 텍스트가 나타날 때까지 기다림
   await page.waitForSelector("text=이름");
   await page.waitForSelector("text=주민등록번호");
+});
+
+test("ocr 기능 테스트 - 신용카드", async ({ page }) => {
+  // index 페이지에서 시작 (playwright.config.ts에서 webServer를 통해 baseURL을 설정.)
+  await page.goto("/");
+
+  // text='신분증 OCR' 텍스트가 포함된 요소를 찾고 클릭
+  await page.click("text=신분증 OCR");
+
+  // url 확인
+  await expect(page).toHaveURL("/detail/ocr");
+
+  // select 선택
+  await page.locator("#\\:r0\\:\\-form-item").click();
+  await page.waitForSelector("[data-radix-popper-content-wrapper]");
+  await page.click("//div[span[text()='신용 카드']]");
+
+  // submit
+  await page.click("button[type='submit']");
+
+  // 결과, 이름, 주민등록번호 텍스트가 나타날 때까지 기다림
+  await page.waitForSelector("text=카드 번호");
+  await page.waitForSelector("text=카드 유효기간");
 });
